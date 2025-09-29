21ce9210a01e07780eb3d392b3a7a1b51b3ca837
ulyaaqilah <ulya.aqilah@gmail.com>
2025-09-29 13:55:14 +0800

    save changes before pull

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\UlasanController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', [UlasanController::class, 'index']);
 Route::post('/ulasan', [UlasanController::class, 'store']);
 
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
+Route::post('/admin/reply/{id}', [AdminController::class, 'reply'])->name('admin.reply');
+Route::delete('/admin/delete/{id}', [AdminController::class, 'destroy'])->name('admin.delete');
